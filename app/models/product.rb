d14a3c06497780ad1e750aa39da1fb3e490618b9
Thomas Brand <tom025@mac.com>
2010-01-22 17:30:11 +0000

    added products to store screen

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,15 @@ class Product < ActiveRecord::Base
   validates_format_of :image_url,
                       :with => %r{\.(gif|jpg|png)$}i,
                       :message => 'must be a url for a gif, jpg or png image.'
+  validates_length_of :tile,
+                      :minimum => 10,
+                      :message => 'Title should be at least 10 characters long'
+
   validate :price_must_be_at_least_a_cent
 
+  def self.find_products_for_sale
+    find(:all, :order => "title")
+  end
   protected
     def price_must_be_at_least_a_cent
       errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
